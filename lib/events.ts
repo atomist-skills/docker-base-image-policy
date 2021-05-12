e968a0e7c8bcc57f109c4815904ef9fcbf767d69
Christian Dupuis <cd@atomist.com>
2021-05-12 11:53:25 +0200

    Delint

--- lib/events.ts
+++ lib/events.ts
@@ -14,22 +14,22 @@
  * limitations under the License.
  */
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 export const pin_from_on_docker_file_on_config_change =
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
 	require("./events/pin_from_on_docker_file").handler;
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 export const validate_base_images_pinned_on_config_change =
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
 	require("./events/validate_base_images_pinned").handler;
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 export const validate_base_images_supported_tag_on_config_change =
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
 	require("./events/validate_base_images_supported_tag").handler;
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 export const validate_linking_on_docker_file_link =
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
 	require("./events/validate_linking_on_docker_file").handler;
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 export const validate_linking_on_docker_image =
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
 	require("./events/validate_linking_on_docker_file").handler;
